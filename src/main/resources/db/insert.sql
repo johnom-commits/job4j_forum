1f2ac41f7a90baf62fa3baca1548e7693bf21023
johnom-commits <johnom2012@yandex.ru>
2021-01-27 12:39:41 +0300

    Add liquibase

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO topics (name, user_id) VALUES ('О чем этот форум?', 1);
+INSERT INTO topics (name, user_id) VALUES ('Правила форума.', 1);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE topics (
+CREATE TABLE IF NOT EXISTS topics (
   id serial PRIMARY KEY,
   name varchar(500) NOT NULL,
   description text,
@@ -6,20 +6,20 @@ CREATE TABLE topics (
   user_id integer NOT NULL REFERENCES users (id)
 );
 
-CREATE TABLE answers (
+CREATE TABLE IF NOT EXISTS answers (
     id serial PRIMARY KEY,
     content text,
     created timestamp without time zone NOT NULL default now(),
     user_id integer NOT NULL REFERENCES users (id)
 );
 
-CREATE TABLE topics_answers (
+CREATE TABLE IF NOT EXISTS topics_answers (
     topic_id integer REFERENCES topics (id),
     answers_id integer REFERENCES answers (id),
     PRIMARY KEY (topic_id, answers_id)
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id serial PRIMARY KEY,
     username varchar(50) NOT NULL UNIQUE,
     password varchar(500) NOT NULL,
